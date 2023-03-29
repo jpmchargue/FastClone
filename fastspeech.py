014816a40563794899bdb0fbc3b6e65dfc4c3511
James McHargue <jpm221@case.edu>
2023-03-29 00:00:49 -0400

    Moving to desktop.

--- fastspeech.py
+++ fastspeech.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+from transformer import FeedForwardTransformer
+
 def positional_encoding_table(k, num_dimensions):
     i = torch.arange(num_dimensions // 2)
     exponential = (10000**((2 * i)/num_dimensions)).expand(k, -1)
@@ -16,9 +18,13 @@ def positional_encoding_table(k, num_dimensions):
 
 
 class Encoder(nn.Module):
-    def __init__(self):
+    def __init__(self, d_model, num_blocks=3):
         super(Encoder, self).__init__()
-        pass
+        
+        self.phoneme_embedding = nn.Embedding(1, d_model)
+        self.transformer = nn.ModuleList(
+            [FeedForwardTransformer(num_heads=2, d_model=d_model) for _ in range(num_blocks)]
+        )
 
     def forward(self, batch):
         pass
