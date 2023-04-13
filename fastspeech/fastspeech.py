e9c1cc64b73bdfac3d806df035efb34a156d0131
James McHargue <jpmchargue@gmail.com>
2023-04-13 00:55:24 -0400

    Removed reference code.

--- fastspeech/fastspeech.py
+++ fastspeech/fastspeech.py
@@ -134,67 +134,6 @@ class VarianceAdaptor(nn.Module):
             output_masks
         )
 
-
-##############
-
-def pad(input_ele, mel_max_length=None):
-    if mel_max_length:
-        max_len = mel_max_length
-    else:
-        max_len = max([input_ele[i].size(0) for i in range(len(input_ele))])
-
-    out_list = list()
-    for i, batch in enumerate(input_ele):
-        if len(batch.shape) == 1:
-            one_batch_padded = F.pad(
-                batch, (0, max_len - batch.size(0)), "constant", 0.0
-            )
-        elif len(batch.shape) == 2:
-            one_batch_padded = F.pad(
-                batch, (0, 0, 0, max_len - batch.size(0)), "constant", 0.0
-            )
-        out_list.append(one_batch_padded)
-    out_padded = torch.stack(out_list)
-    return out_padded
-
-class LengthRegulator(nn.Module):
-    """Length Regulator"""
-
-    def __init__(self):
-        super(LengthRegulator, self).__init__()
-
-    def LR(self, x, duration, max_len):
-        output = list()
-        mel_len = list()
-        for batch, expand_target in zip(x, duration):
-            expanded = self.expand(batch, expand_target)
-            output.append(expanded)
-            mel_len.append(expanded.shape[0])
-
-        if max_len is not None:
-            output = pad(output, max_len)
-        else:
-            output = pad(output)
-
-        return output, torch.LongTensor(mel_len).to(device)
-
-    def expand(self, batch, predicted):
-        out = list()
-
-        for i, vec in enumerate(batch):
-            expand_size = predicted[i].item()
-            out.append(vec.expand(max(int(expand_size), 0), -1))
-        out = torch.cat(out, 0)
-
-        return out
-
-    def forward(self, x, duration, max_len):
-        output, mel_len = self.LR(x, duration, max_len)
-        return output, mel_len
-    
-###################
-
-
 class VariancePredictor(nn.Module):
     def __init__(self, d_model, d_hidden=256, dropout=0.5):
         super(VariancePredictor, self).__init__()
